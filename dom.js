1c50ff9e66f31c641f2aa9d57f94ac482de39995
brad traversy <brad@traversy.com>
2022-02-08 17:46:18 +0530

    initial commit

---
+++ dom.js
@@ -0,0 +1,28 @@
+// Examine the document object //
+// console.log(document);
+
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// // document.title =123;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// //document.all[10].textContent = "hello" ;
+// console.log(document.forms[0]);
+// console.log(document.links);
+
+//console.log(document.images);
+
+// getelemented //
+//console.log(document.getElementById('header-title'));
+var headerTitle = document.getElementById('header-title');
+//console.log(headerTitle);
+// headerTitle.textContent = 'hello';
+// headerTitle.innerText = 'Goodbye';
+//console.log(headerTitle.innerText);
+//headerTitle.innerHTML = ' <h3>hello</h3>';
+
+
